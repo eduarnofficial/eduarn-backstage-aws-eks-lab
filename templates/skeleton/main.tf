9415b2f2f169640f9cf5d6e6dbd9d273359f10b7
EduArn <eduarnofficial@gmail.com>
2026-05-24 19:35:58 +0530

    Add files via upload

---
+++ templates/skeleton/main.tf
@@ -0,0 +1,199 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  default = "eduarn-dev"
+}
+
+
+# -----------------------------
+# VPC
+# -----------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+# -----------------------------
+# SUBNET 1
+# -----------------------------
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-subnet1"
+  }
+}
+
+# -----------------------------
+# SUBNET 2
+# -----------------------------
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-subnet2"
+  }
+}
+
+# -----------------------------
+# INTERNET GATEWAY
+# -----------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# -----------------------------
+# ROUTE TABLE
+# -----------------------------
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# -----------------------------
+# ROUTE TABLE ASSOCIATIONS
+# -----------------------------
+resource "aws_route_table_association" "subnet1_assoc" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "subnet2_assoc" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# -----------------------------
+# EKS CLUSTER IAM ROLE
+# -----------------------------
+resource "aws_iam_role" "eks_role" {
+  name = "${var.project_name}-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# -----------------------------
+# EKS CLUSTER POLICY
+# -----------------------------
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# -----------------------------
+# EKS CLUSTER
+# -----------------------------
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.project_name}-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet1.id,
+      aws_subnet.subnet2.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy
+  ]
+}
+
+# -----------------------------
+# NODE GROUP IAM ROLE
+# -----------------------------
+resource "aws_iam_role" "node_role" {
+  name = "${var.project_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# -----------------------------
+# NODE IAM POLICIES
+# -----------------------------
+resource "aws_iam_role_policy_attachment" "node_policy_1" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_2" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_3" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# -----------------------------
+# EKS NODE GROUP
+# -----------------------------
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "dev-nodes"
+
+  node_role_arn = aws_iam_role.node_role.arn
+
+  subnet_ids = [
+    aws_subnet.subnet1.id,
+    aws_subnet.subnet2.id
+  ]
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy_1,
+    aws_iam_role_policy_attachment.node_policy_2,
+    aws_iam_role_policy_attachment.node_policy_3
+  ]
+}
